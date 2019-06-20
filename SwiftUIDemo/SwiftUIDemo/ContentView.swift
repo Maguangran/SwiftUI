44df58dc045045b7d3aeef643d847d8bfddea38b
mgr <guangran.ma@at.top>
2019-06-20 13:51:50 +0800

    init

---
+++ SwiftUIDemo/SwiftUIDemo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIDemo
+//
+//  Created by mgr on 2019/6/20.
+//  Copyright © 2019 mgggr. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
